e5a9d25fbcfe6fdeb8ac07b4ff2f4bf2f614a945
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-15 09:36:15 +0200

    asked user for his age and calculated how old he will be in X years #5

--- beginning_c_sharp/beginning_c_sharp/Program.cs
+++ beginning_c_sharp/beginning_c_sharp/Program.cs
@@ -12,10 +12,19 @@ namespace beginning_c_sharp
         { 
             Console.WriteLine("What´s your name?");
             string userName = Console.ReadLine();
+
             Console.WriteLine("Hello " + userName + "!");
-            Console.WriteLine("How old are youß");
+
+            Console.WriteLine("How old are you?");
             int age = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("You were born in " + Convert.ToString(2022 - age));
+
+            Console.WriteLine("How old will you be in X years?");
+            int nextYear = Convert.ToInt32(Console.ReadLine());
+            int sumYear = age + nextYear;
+
+            Console.WriteLine("In " + nextYear + " years you will be " + sumYear + ". "); 
 
 
             Console.ReadLine();
